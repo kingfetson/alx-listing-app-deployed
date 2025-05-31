29e2e7ce2dc84bd5e58d61aa4c5914764be42fe2
Festus Kimani <171272139+kingfetson@users.noreply.github.com>
2025-05-31 12:27:48 +0300

    Update index.ts

--- constants/index.ts
+++ constants/index.ts
@@ -1,2 +1,23 @@
-export const PLACEHOLDER_IMAGE = '/assets/placeholder.png';
-export const SITE_NAME = 'ALX Listing App';
+import { PropertyProps } from "@/interfaces";
+
+export const PROPERTYLISTINGSAMPLE: PropertyProps[] = [
+  {
+    name: "Villa Ocean Breeze",
+    address: {
+      state: "Seminyak",
+      city: "Bali",
+      country: "Indonesia",
+    },
+    rating: 4.89,
+    category: ["Luxury Villa", "Pool", "Free Parking"],
+    price: 3200,
+    offers: {
+      bed: "3",
+      shower: "3",
+      occupants: "4-6",
+    },
+    image: "https://example.com/image1.jpg",
+    discount: "",
+  },
+  // ... rest of the 19 listings here
+];
